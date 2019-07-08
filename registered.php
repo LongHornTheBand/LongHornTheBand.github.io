dfb825b88ad1e40a69caf752e1ec4199d61e29f1
LongHornTheBand <52625873+LongHornTheBand@users.noreply.github.com>
2019-07-08 11:40:34 +0200

    Add files via upload

--- registered.php
+++ registered.php
@@ -3,11 +3,11 @@
 	<head>
 		<link href="images/icon.png" rel="icon" type="image/x-icon"/> 
 		<link id="pagestyle" rel="stylesheet" href="css/final.css">
-		<title>Symposium E.S.C.</title>
+		<title>Long Horn</title>
 		<meta name="robots" content="noindex,nofollow"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<meta charset="UTF-8"/>
-		<link rel="icon" href="images/logo.png">
+		<link rel="icon" href="images/longhornlogo.png">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link id="pagestyle" rel="stylesheet" href="css/final.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -22,50 +22,49 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="index.html"><img src="images/logo.png" class="logo" alternative="logo"></a>
+					<a class="navbar-brand" href="index.html"><img src="images/longhornlogo.png" class="logo" alternative="logo"></a>
 				</div>
 				<div class="collapse navbar-collapse" id="myNavbar">
 					<ul class="nav navbar-nav">
 						<li><a href="index.html">Home</a></li>
-						<li><a href="program.html">Program</a></li>
-						<li><a href="keynotespeakers.html">Keynote Speakers</a></li>
-						<li><a href="location.html">Location</a></li>
+						<li><a href="program.html">Programma</a></li>
 						<li><a href="sponsors.html">Sponsors</a></li>
 						<li><a href="contact.html">Contact</a></li>
-						<li class="active"><a href="register.html">Register</a></li>
-						<li><a href="aboutus.html">About Us</a></li>
+						<li class="active"><a href="register.html">Registeer</a></li>
+						<li><a href="aboutus.html">Info</a></li>
 					</ul>
 				</div>
 			</div>
 		</nav>
 		<header class="container">
-			<h1>Succesfully registered</h1>
+			<h1>Succesvol geregistreerd</h1>
 		</header>
 		<div class="container">
 			<p>
-				Name: 
+				Naam: 
 				<?php echo htmlspecialchars($_POST["name"]);?>
 			</p>
 			<p>
 				E-mail:
 				<?php echo htmlspecialchars($_POST["email"]);
-				mail($_POST["email"],"E.S.C. Symposium", "Dear ".htmlspecialchars($_POST["name"])."\n\nYou have succesfully registered for the symposium on the 13th of april 2019.\n\nSee you there!\n\nThe Electronic Speakers Collective");?>
+				mail($_POST["email"],"Long Horn The Band", "Beste ".htmlspecialchars($_POST["name"])."\n\nU bent succesvol geregistreerd.\n\nTot dan!\n\nLong Horn the band");
+				mail($_POST["longhorntheband@gmail.com"],"Long Horn The Band", "E-mail verstuurd:\n\nBeste ".htmlspecialchars($_POST["name"])."\n\nU bent succesvol geregistreerd.\n\nTot dan!\n\nLong Horn the band\n\nInfo voor Long Horn:\n".htmlspecialchars($_POST["email"])."\n".htmlspecialchars($_POST["phone"])."\n".htmlspecialchars($_POST["remarks"]));?>
 			</p>
 			<p>
-				Phone:
+				Telefoon:
 				<?php if (!empty($_POST["phone"])) { echo htmlspecialchars($_POST["phone"]);} else {echo "-";}?>
 			</p>
 			<p>
-				Remarks:
+				Opmerkingen:
 				<?php if (!empty($_POST["remarks"])) { echo htmlspecialchars($_POST["remarks"]);} else {echo "-";}?>
 			</p>
 			<p>
-				You have received an e-mail as a reminder. See you on the 13th of april!
+				U heeft een e-mail van ons ontvangen.
 			</p>
-			<a href="register.html">Back</a>
+			<a href="register.html">Terug</a>
 		</div>
 		<footer class="footer">
-			Chamber of Commerce 99.95.00.1B.27
+			Long Horn
 		</footer>
 	</body>
 </html>
